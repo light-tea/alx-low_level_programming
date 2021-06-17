1db7c7d685950d1a625f572eb4f2d55b0e2efa05
Light-name <tosinemmaakin@gmail.com>
2021-06-17 12:22:00 -0700

    initial commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,8 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void) {
-	puts ("Programming is like building a multilingual puzzle\n");
+int main(void) 
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
