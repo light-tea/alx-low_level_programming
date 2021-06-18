3bc0acc51d1f1c7abc60226eb9ddc2557f443098
Light-name <tosinemmaakin@gmail.com>
2021-06-18 11:38:18 -0700

    C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet in lower and uppercase
+ *
+ *
+ * Return: (0) Success
+ */
+int main(void)
+{
+	char a;
+{
+        char a;
+        for (a = 'a' ; a = 'z' ; a++)
+{
+        putchar(a);
+}
+        for (a = 'A' ; a = 'Z' ; a++)
+{
+        putchar(a);
+}
+        putchar('\n');
+        return (0);
+}
