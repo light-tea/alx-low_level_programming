7b3804500af27e056c3e28ee30b4f5fe5f86decf
Light-name <tosinemmaakin@gmail.com>
2021-06-18 05:07:21 -0700

    initial commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,11 +13,17 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if(n<0){
-	printf("is negative");
-	}else if(n==0){
-	printf("is zero");
-	}else if(n>0){
-	}printf("is positive");
+	if (n < 0)
+	{
+	printf("is negative\n");
+	}
+	else if ( n == 0 )
+	{
+	printf("is zero\n");
+	}
+	else if ( n > 0)
+	{
+	printf("is positive\n");
+	}
 	return (0);
 }
