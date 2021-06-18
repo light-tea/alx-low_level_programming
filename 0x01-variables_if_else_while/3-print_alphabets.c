5e8a90e86c77e093c738e3c047011d0429339276
Light-name <tosinemmaakin@gmail.com>
2021-06-18 12:28:45 -0700

    C Programming ifs state

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,16 +5,13 @@
 * Return: Always 0 (Success)
 */
 
-
 int main(void)
 {
 	char tea;
-
 	for (tea = 'a'; tea <= 'z'; tea++)
 	putchar(tea);
 	for (tea = 'A'; tea <= 'Z'; tea++)
 	putchar(tea);
 	putchar('\n');
-
 	return (0);
 }
