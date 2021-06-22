15e79e83663c7774499f59cefcd45d0aa01080bb
Light-name <tosinemmaakin@gmail.com>
2021-06-22 11:55:20 -0700

    low level

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -12,9 +12,10 @@ int main(void)
 	char holberton[9] = "Holberton";
 	int i;
 	for (i = 0; i < 9; i++)
-{
-	_putchar(holberton[i]);
+
 }	
+	_putchar(holberton[i]);
+}
 	_putchar('\n');
 	
 	return (0);
