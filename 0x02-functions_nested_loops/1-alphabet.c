789f6c1c2c6931c7cc3f3924a6edadc67936edf5
Light-name <tosinemmaakin@gmail.com>
2021-06-22 11:59:56 -0700

    alphabets print

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * print_alphabet - Function that prints alphabet in lowercase
+ */
+
+void print_alphabet(void)
+{
+	
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	_putchar(ch);
+	_putchar('\n');
+}
