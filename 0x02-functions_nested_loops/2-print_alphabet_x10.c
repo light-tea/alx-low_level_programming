2e4523c4d85488e3195777a09634eb5117819aeb
Light-name <tosinemmaakin@gmail.com>
2021-06-22 12:17:29 -0700

    print alphabets

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,7 +7,7 @@ void print_alphabet_x10(void)
 {
 
 	int count;
-	
+
 	char alp;
 
 	for (count = 0; count <= 9; count++)
@@ -15,7 +15,7 @@ void print_alphabet_x10(void)
 	for (alp = 'a'; alp <= 'z'; alp++)
 {
 	_putchar(alp);
-}	
+}
 	_putchar('\n');
 }
 }
