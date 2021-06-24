3b7ec86254d69c61ecfe1f120b7a0a86beb76bbb
Light-name <tosinemmaakin@gmail.com>
2021-06-24 10:20:57 -0700

    initial

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -7,4 +7,16 @@ int _isupper(int c);
 /*prototype for is digit*/
 int _isdigit(int c);
 
+int _putchar(char c);
+
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+
 #endif
