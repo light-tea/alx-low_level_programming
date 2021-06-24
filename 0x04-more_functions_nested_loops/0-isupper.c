15e37aca2763b3344414a75485d06f220622c805
Light-name <tosinemmaakin@gmail.com>
2021-06-24 06:00:37 -0700

    lower case

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+
+	int _isupper(int c){
+	    
+	if ( c >= 'a' && c <= 'z')
+{
+
+	return(0);
+}	
+	else
+{
+	return(1);
+}
+}
