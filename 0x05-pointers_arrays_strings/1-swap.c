478bf341662de298cc388ea22a1bc13a55cf7ae7
Light-name <tosinemmaakin@gmail.com>
2021-06-28 05:49:43 -0700

    swap intergers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,7 +1,8 @@
 #include "holberton.h"
 #include <stdio.h>
 
-/** swap_int - a function that swaps values of two integers
+/** 
+ * swap_int - a function that swaps values of two integers
  *
  * @a: An integer to swap
  * @b: Another integer to swap
