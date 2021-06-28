ad00efc773a25dd6ba11d78e46a5095569de4fda
Light-name <tosinemmaakin@gmail.com>
2021-06-28 05:40:40 -0700

    Swap values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/** void swap_int - A function that swaps values of two integers
+ *
+ * @x: An integer to swap
+ * @y Another integer to swap
+ *
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int swp;
+
+	swp = *x;
+	*x = y;
+	*y = swp;
+}
