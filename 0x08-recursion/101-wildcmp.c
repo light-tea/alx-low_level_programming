cf6ccb72a28e2305f256f35be2d91c8f2eed39c9
Light-name <tosinemmaakin@gmail.com>
2021-07-07 09:14:55 -0700

    advance

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,30 +1,23 @@
 #include "holberton.h"
 
 /**
- * wildcmp - compares two strings
- * @s1: first string
- * @s2: second string, may contain wildcard *
- *
- * Return: 1 if match, 0 if not
+ * wildcmp - compares two strings and returns 1 if the strings can be
+ * considered identical, otherwise return 0
+ * @s1: input string1
+ * @s2: input string2
+ * Return: 1 if true, 0 if false
  */
 int wildcmp(char *s1, char *s2)
 {
-	char *p;
-
-	if (*s2 == '*' && *(s2 + 1) == '\0')
-		return (1);
-	else if (*s2 == '*' && *(s2 + 1) == '*')
-		return (wildcmp(s1, s2 + 1));
-	else if (*s2 == '*')
-	{
-		p = wildcard(s1, s2 + 1);
-		if (p == 0)
-			return (0);
-		return (wildcmp(p + 1, s2 + 2));
+if (*s1 == '\0')
+{
+if (*s2 != '\0' && *s2 == '*')
+return (wildcmp(s1, s2 + 1));
+return (*s2 == '\0');
 	}
-	else if (*s1 != *s2)
-		return (0);
-	if (*s1 == '\0' && *s2 == '\0')
-		return (1);
-	return (wildcmp(s1 + 1, s2 + 1));
+if (*s2 == '*')
+return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+else if (*s1 == *s2)
+return (wildcmp(s1 + 1, s2 + 1));
+return (0);
 }
