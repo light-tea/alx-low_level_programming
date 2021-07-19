17c4f158b8da91e4f4edc092a427fabe6303316c
Light-name <tosinemmaakin@gmail.com>
2021-07-19 05:32:47 -0700

    print name of file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point, prints filename using __FILE__ macro
+ *
+ * Return: 0 (Success)
+ */
+
+int main()
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
