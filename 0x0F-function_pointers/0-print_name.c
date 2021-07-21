3b50fb0aa0c8abbb8cb25cbe089b2776e2653266
Light-name <tosinemmaakin@gmail.com>
2021-07-21 05:23:49 -0700

    initial

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,14 @@
 #include "function_pointers.h"
-#include <stdlib.h>
 
 /**
  * print_name - prints a name
- * @name: string name
- * @f: callback
- * Return: void
+ * @name: name to be printed
+ * @f: callback function that prints name
  */
-
 void print_name(char *name, void (*f)(char *))
 {
-	if (f != NULL)
-		f(name);
+	if (!f)
+		return;
+	(*f)(name);
+
 }
