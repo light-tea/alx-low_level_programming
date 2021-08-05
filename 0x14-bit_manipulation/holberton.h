1242e547043a50cb14873eaeeaf528fab3f468bb
Light-name <tosinemmaakin@gmail.com>
2021-08-05 14:05:55 -0700

    fix

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -1,5 +1,5 @@
-#ifndef HEADER_H
-#define HEADER_H
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
