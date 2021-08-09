34ff4840b90f3e6c1f1e58cc44ae8fb3da5cdc25
Light-name <tosinemmaakin@gmail.com>
2021-08-09 13:13:12 -0700

    fix

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -10,31 +10,36 @@
 
 int main(int argc, char *argv[])
 {
-int dest;
-ssize_t size;
-char buffer[5];
-char elf[1];
-if (argc != 2)
-{
-dprintf(STDERR_FILENO, "Usage: cp file\n");
-exit(97);
-}
-dest = open(argv[1], O_RDWR);
-if (dest == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't open from file %s\n", argv[1]);
-exit(98);
-}
-size = read(dest, buffer, 4);
-if (size == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-exit(98);
-}
-size = lseek(dest, 1, SEEK_SET);
-size = read(dest, elf, 1);
-if (elf[0] != 'E')
-exit(98);
-close(dest);
-return (0);
+	int dest;
+	ssize_t size;
+	char buffer[5];
+	char elf[1];
+
+	if (argc != 2)
+	{
+	dprintf(STDERR_FILENO, "Usage: cp file\n");
+	exit(97);
+	}
+	
+	dest = open(argv[1], O_RDWR);
+	if (dest == -1)
+	{
+
+	dprintf(STDERR_FILENO, "Error: Can't open from file %s\n", argv[1]);
+	exit(98);
+	}
+	size = read(dest, buffer, 4);
+	if (size == -1)
+	{
+	
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+	exit(98);
+	}
+	
+	size = lseek(dest, 1, SEEK_SET);
+	size = read(dest, elf, 1);
+	if (elf[0] != 'E')
+	exit(98);
+	close(dest);
+	return (0);
 }
